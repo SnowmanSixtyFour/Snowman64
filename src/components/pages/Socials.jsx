0d353a9dfa086bcbd585ff72a5c99dc7274f48a4
SnowmanSixtyFour <162073017+SnowmanSixtyFour@users.noreply.github.com>
2026-07-22 15:32:14 -0400

    Finished Socials Page

--- src/components/pages/Socials.jsx
+++ src/components/pages/Socials.jsx
@@ -1,9 +1,21 @@
 import { useState } from 'react'
+import { Link } from "react-router-dom";
 
 function Socials() {
   return (
     <div>
-      <p>Socials test</p>
+      <p><b>Here are each of my accounts on different social media platforms.</b></p>
+      <br/>
+      <ul>
+        <li><a href="https://bit.ly/snowman64" target="_blank" rel="noopener noreferrer">YouTube</a></li>
+        <li><a href="https://bsky.app/profile/snowman64.com" target="_blank" rel="noopener noreferrer">Bluesky</a></li>
+        <li><a href="https://www.twitch.tv/snowmansixtyfour" target="_blank" rel="noopener noreferrer">Twitch</a></li>
+        <li><a href="https://www.tiktok.com/@_snowman64_" target="_blank" rel="noopener noreferrer">TikTok</a></li>
+        <li><a href="https://www.instagram.com/_snowman64_/" target="_blank" rel="noopener noreferrer">Instagram</a></li>
+        <li><a href="https://www.reddit.com/user/_snowman64_/" target="_blank" rel="noopener noreferrer">Reddit</a></li>
+      </ul>
+      <br/>
+      <Link className="navButton" to="/">Go Back</Link>
     </div>
   )
 }
